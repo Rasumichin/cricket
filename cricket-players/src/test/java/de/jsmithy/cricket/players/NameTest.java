7b77f1bf3dd7afa5592f37920bcd449202e142dc
Erik Lotz <elo@jsmithy.de>
2018-05-01 16:34:23 +0200

    GH-1 Add JUnit dependency and very first test stub.

---
+++ cricket-players/src/test/java/de/jsmithy/cricket/players/NameTest.java
@@ -0,0 +1,24 @@
+package de.jsmithy.cricket.players;
+
+import static org.junit.Assert.*;
+
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+/**
+ * Test case for the domain attribute 'Name' of a player.
+ * 
+ * @author erik
+ *
+ */
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class NameTest {
+
+	@Ignore
+	@Test
+	public void test() {
+	}
+}
