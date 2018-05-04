44805c96f629d89a8588ed2f668841c19fb6cf0c
Erik Lotz <elo@jsmithy.de>
2018-05-04 22:57:22 +0200

    GH-1 Implement basic functionality for a 'Name' type.
    Does not handle null values so far. Will implement NullObject pattern.

---
+++ cricket-players/src/main/java/de/jsmithy/cricket/players/Name.java
@@ -0,0 +1,59 @@
+package de.jsmithy.cricket.players;
+
+import java.io.Serializable;
+
+/**
+ * Player's Name domain attribute. A name may consist of a first name, a surname or both.
+ * <li>Use the factory method {@link #newInstance(String)} to create instances.</li>
+ * <li>Supports the "NullObject" pattern.</li>
+ * 
+ * @author Erik
+ *
+ */
+public class Name implements Serializable {
+	private static final long serialVersionUID = 1140989109643131931L;
+
+	private String value;
+
+	private Name(String name) {
+		value = name;
+	}
+
+	public static Name newInstance(String name) {
+		return new Name(name);
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Name other = (Name) obj;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return value;
+	}
+}
--- cricket-players/src/test/java/de/jsmithy/cricket/players/NameTest.java
+++ cricket-players/src/test/java/de/jsmithy/cricket/players/NameTest.java
@@ -2,23 +2,73 @@ package de.jsmithy.cricket.players;
 
 import static org.junit.Assert.*;
 
+import java.io.Serializable;
+
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 /**
  * Test case for the domain attribute 'Name' of a player.
  * 
- * @author erik
+ * @author Erik
  *
  */
-
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class NameTest {
 
-	@Ignore
 	@Test
-	public void test() {
+	public void testNewInstance_with_name_value() {
+		String name = "Phil Taylor";
+		
+		Name sut = Name.newInstance(name);
+		
+		assertNotNull("Instance has not been created!", sut);
+	}
+
+	@Test
+	public void testToString_with_name_value() {
+		String expected = "Phil Taylor";
+		Name sut = Name.newInstance(expected);
+		
+		String actual = sut.toString();
+		
+		assertEquals("String representation is not correct!", expected, actual);
+	}
+	
+	@Test
+	public void testIsEmpty_with_name_value() {
+		String name = "Phil Taylor";
+		Name sut = Name.newInstance(name);
+		
+		boolean condition = sut.isEmpty();
+		
+		assertFalse("Name with value must not be empty!", condition);
+	}
+	
+	@Test
+	public void testEquals_when_true() {
+		Name nameA = Name.newInstance("Phil Taylor");
+		Name nameB = Name.newInstance("Phil Taylor");
+		
+		assertEquals("Instances are not equal!", nameA, nameB);
+	}
+
+	@Test
+	public void testEquals_when_false() {
+		Name nameA = Name.newInstance("Phil Taylor");
+		Name nameB = Name.newInstance("Wayne Mardle");
+		
+		assertNotEquals("Instances are equal!", nameA, nameB);
+	}
+
+	@Test
+	public void testIsSerializable() {
+		String name = "Phil Taylor";
+		Name sut = Name.newInstance(name);
+		
+		boolean condition = sut instanceof Serializable;
+		
+		assertTrue("Instance is not serializable!", condition);
 	}
 }
